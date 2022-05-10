a8abd69e1b74223afd53c9866fef1d1dfc99a45d
wesley <wesbourkeavl@gmail.com>
2022-05-09 20:13:14 -0400

    working on front end authorization

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,27 +1,28 @@
 import React, {useState} from 'react'
 import "../App.css"
 
-
-function Login() {
-    const [usrForm, setUsrForm] = useState({
-        usrName: "",
-        pasWord: ""  
-    })
+function Login({ loginFunction}) {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
     
     function formSubmit(event) {
         event.preventDefault()
-        if (usrForm.usrName !== "" && usrForm.pasWord !== "") {
-            console.log(usrForm)
+        console.log(username, password)
+        if (username && password) {
+            loginFunction(username, password)
         }
-        else if (usrForm.usrName === "") {
+        else if (username === "") {
             alert("Please enter a user Name.")
         }
         else {
             alert("Please enter a user Password.")
         }
     }
-    function changeStateFunction(event) {
-        setUsrForm({...usrForm, [event.target.name]: event.target.value})
+    function changeUsername(event) {
+        setUsername(event.target.value)
+    }
+    function changePassword(event) {
+        setPassword(event.target.value)
     }
     function modalPopUp() {
         console.log("worked!")
@@ -32,15 +33,16 @@ function Login() {
     function ReturnBackground(e) {
         e.target.style.background = 'white';
     }
+    
 
   return (
     <div className="flex-container" id="pageContainerDiv">
        <div className="flex-container" id="divSignInForm">
            <form onSubmit={formSubmit}>
                 <label>User Name:</label>    
-                <input type="text" name="usrName" onChange={changeStateFunction} value={usrForm.usrName} />
+                <input type="text" name="usrName" onChange={changeUsername} value={username} />
                 <label>Password:</label>  
-                <input type="text" name="pasWord" onChange={changeStateFunction} value={usrForm.pasWord} />
+                <input type="text" name="pasWord" onChange={changePassword} value={password} />
                 <button type="submit">[X]</button>
             </form>   
             <div className="modal" onClick={modalPopUp} onMouseEnter={changeBackground} onMouseLeave={ReturnBackground}>Don't have an account? Sign up</div>
