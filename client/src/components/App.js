1d6b0eca915efef88fbc0cc8e8736c4932bae304
wesley <wesbourkeavl@gmail.com>
2022-05-11 15:50:50 -0400

    started to add the post for create new progy

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,12 +3,9 @@ import React, { useEffect, useState } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from './Login';
 import AddProject from './AddProject';
-<<<<<<< HEAD
 import SingleProjectView from './SingleProjectView';
 import ClientView from './ClientView';
-=======
 import Header from './Header';
->>>>>>> wesBranchMay11
 
   function App() {
     const [currentUser, setCurrentUser] = useState([])
@@ -16,21 +13,29 @@ import Header from './Header';
     /////// Caleb - working on current project view
     const [currentProject, setCurrentProject] = useState([])
     /////// Caleb - working on current project view
-    
+    const [photoProjects, setPhotoProjects] = useState([])
     let navigate = useNavigate();
+//////////////////////////////////////////////////////////////////
+  useEffect(() => {
+    fetch(`http://localhost:3000/projects`)
+    .then( res => res.json())
+    .then( data => setPhotoProjects(data))
+    .catch( error => console.log(error.message)
+  )}, [])   
 
-   useEffect(() => {
-      fetch('/auth')
-      .then(res => {
-        if(res.ok){
-          res.json().then(user => { 
-            setCurrentUser(user) 
-            setCurrentUserProjects(user.projects)
-          })
-        }
-      })
-    }, [])
-
+  useEffect(() => {
+        fetch('/auth')
+        .then(res => {
+          if(res.ok){
+            res.json().then(user => { 
+              setCurrentUser(user) 
+              setCurrentUserProjects(user.projects)
+            })
+          }
+        })
+      }, [])
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
     function loginFunction(username, password) {
       fetch(`/login`, {
           method: "POST",
@@ -51,26 +56,8 @@ import Header from './Header';
       })
       .catch( error => console.log(error.message));
     }
-
-<<<<<<< HEAD
-  // const [photoProjects, setPhotoProjects] = useState([])
-  //   useEffect(() => {
-  //     fetch(`http://localhost:3000/projects`)
-  //     .then( res => res.json())
-  //     .then( data => setPhotoProjects(data))
-  //     .catch( error => console.log(error.message)
-  //   )}, [])
-
-    return (
-=======
-  const [photoProjects, setPhotoProjects] = useState([])
-    useEffect(() => {
-      fetch(`http://localhost:3000/projects`)
-      .then( res => res.json())
-      .then( data => setPhotoProjects(data))
-      .catch( error => console.log(error.message)
-    )}, [])
-    
+//////////////////////////////////////////////////////////////////    
+//////////////////////////////////////////////////////////////////
     function logUserOut() {
       fetch(`/logout`, {
           method: "DELETE"
@@ -79,7 +66,8 @@ import Header from './Header';
       setCurrentUser([])
       navigate(`/`)
     }
-
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
     function postNewUser(newUser) {
       fetch(`/clients`, {
           method: "POST",
@@ -95,20 +83,30 @@ import Header from './Header';
       .then( data => console.log(data))
       .catch( error => console.log(error.message));
    }
-    
+
+   function postNewProjy(newProjy) {
+    fetch(`/projects`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(
+            newProjy
+        )
+    })
+    .then( res => res.json())
+    .then( data => console.log(data))
+    .catch( error => console.log(error.message));
+ }
+//////////////////////////////////////////////////////////////////////
   return (
->>>>>>> wesBranchMay11
     <div>
         {currentUser.id ? <Header logUserOut={logUserOut}/> : null}
           <Routes>
-<<<<<<< HEAD
-            <Route path="/" element={<Login loginFunction={loginFunction} />}></Route>
-            <Route path="/my_projects" element={<ClientView currentProject={currentProject} setCurrentProject={setCurrentProject} currentUserProjects={currentUserProjects} currentUser={currentUser}/>} />
-=======
             <Route path="/" element={<Login loginFunction={loginFunction} postNewUser={postNewUser}/>}></Route>
-            <Route path="/my_projects" element={<ClientStatusCards currentUserProjects={currentUserProjects} currentUser={currentUser}/>} />
->>>>>>> wesBranchMay11
-            <Route path="/add_project" element={<AddProject />} />
+            <Route path="/my_projects" element={<ClientView currentProject={currentProject} setCurrentProject={setCurrentProject} currentUserProjects={currentUserProjects} currentUser={currentUser}/>} />
+            <Route path="/add_project" element={<AddProject postNewProjy={postNewProjy} />} />
             <Route path="/my_projects/:id" element={<SingleProjectView currentProject={currentProject}/>} /> 
           </Routes>
     </div>
