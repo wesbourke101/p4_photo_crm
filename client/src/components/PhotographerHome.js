d413862bf4e549fe836abb4bac381bde928577b8
wesley <wesbourkeavl@gmail.com>
2022-05-12 16:57:27 -0400

    fixed reRenders

--- client/src/components/PhotographerHome.js
+++ client/src/components/PhotographerHome.js
@@ -15,14 +15,30 @@ function PhotographerHome({currentUserProjects, currentUser, setCurrentProject})
   const mappedCompleted = completed.map((projects) => { return <SingleProjectView key={projects.id} project={projects} setCurrentProject={setCurrentProject}/>})
   const mappedPending = pending.map((projects) => { return <SingleProjectView key={projects.id} project={projects} setCurrentProject={setCurrentProject}/>})
   const mappedProgress = inProgress.map((projects) => { return <SingleProjectView key={projects.id} project={projects} setCurrentProject={setCurrentProject}/>})
-
+  console.log(currentUser)
   return (
     <div>
-      {mappedCompleted}
-      <>---------------------------------</><br/>
-      {mappedPending}
-      <>---------------------------------</><br/>
-      {mappedProgress}
+      <div>
+        -----------------------------------------
+        <h2>{currentUser.first_name} {currentUser.last_name}</h2>
+        <text>Photographer</text><br/>
+        <p>Bio:</p>
+        <p>{currentUser.bio}</p>
+
+        <a href={currentUser.website}>{currentUser.first_name}'s website</a><br/>
+        -----------------------------------------
+      </div>  
+      <div>
+        {mappedCompleted}
+      </div>  
+        <>---------------------------------</><br/>
+      <div>  
+        {mappedPending}
+      </div>    
+        <>---------------------------------</><br/>
+      <div>  
+        {mappedProgress}
+      </div>
     </div>
    )
 
