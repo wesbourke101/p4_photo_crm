d011652dabfc48190bd88dfea7ce73482c5779a3
wesley <wesbourkeavl@gmail.com>
2022-05-13 13:49:37 -0400

    wes

--- client/src/components/AddProject.js
+++ client/src/components/AddProject.js
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 
 function AddProject({postNewProjy, currentUser}) {
   ///////////////////////////////////////////////////////////////////
   const [photoGraphers, setPhotoGraphers] = useState([])
-  
+  const location = useLocation();
+  console.log(location)
 
   let navigate = useNavigate();
 
@@ -42,7 +43,7 @@ function AddProject({postNewProjy, currentUser}) {
   const photographerMappedOptions = photoGraphers.map((photoOption) => {return <option value={photoOption.id} >{photoOption.first_name} {photoOption.last_name}</option> })
   return (
     <div className="user-card" >
-
+      
         <div className="user-card">
             <div>
                 <form onSubmit={createNewProject} id="classicCards" className="grid grid-rows-4 grid-flow-col gap-4">
